3cc6e5923aefa1fb95a2b83effc360be9f9229b3
Alexei Zababurin <852gin@gmail.com>
2019-01-13 17:38:02 +0200

    adding built code

---
+++ build/precache-manifest.daf440194f5c1b9086ec8acb2fed7d0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7ecf20df02f1455c9ee6691cd7ab707",
+    "url": "/Hydraulicus/liquidflow/flow_animation/static/media/pipe_rear.c7ecf20d.svg"
+  },
+  {
+    "revision": "476087bf08debb3fc8b24ab588e2a38d",
+    "url": "/Hydraulicus/liquidflow/flow_animation/static/media/pipe_front.476087bf.svg"
+  },
+  {
+    "revision": "d97420ae21db14b98463",
+    "url": "/Hydraulicus/liquidflow/flow_animation/static/js/runtime~main.d97420ae.js"
+  },
+  {
+    "revision": "25d399f7319abce85b91",
+    "url": "/Hydraulicus/liquidflow/flow_animation/static/js/main.25d399f7.chunk.js"
+  },
+  {
+    "revision": "6544fe970ae5cf82e66e",
+    "url": "/Hydraulicus/liquidflow/flow_animation/static/js/1.6544fe97.chunk.js"
+  },
+  {
+    "revision": "25d399f7319abce85b91",
+    "url": "/Hydraulicus/liquidflow/flow_animation/static/css/main.7f357b80.chunk.css"
+  },
+  {
+    "revision": "12c3e5f93e0c09c082a6f6c09c81546b",
+    "url": "/Hydraulicus/liquidflow/flow_animation/index.html"
+  }
+];
