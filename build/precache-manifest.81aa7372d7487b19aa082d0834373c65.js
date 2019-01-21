af72e57b07f3b7d807633897dd743d7d51ec6274
Alexei Zababurin <852gin@gmail.com>
2019-01-22 01:02:15 +0200

    Refining color of pipe Adding UI elements of flow options

---
+++ build/precache-manifest.81aa7372d7487b19aa082d0834373c65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb3cf08db954797fe0726771f0f6a7b0",
+    "url": "/liquidflow/static/media/pipe_rear.bb3cf08d.svg"
+  },
+  {
+    "revision": "3f757fac70c645c9195fe98a0993df7d",
+    "url": "/liquidflow/static/media/pipe_front.3f757fac.svg"
+  },
+  {
+    "revision": "c5c97a7d2191561ebdd0",
+    "url": "/liquidflow/static/js/runtime~main.c5c97a7d.js"
+  },
+  {
+    "revision": "a9124e4cfa7a459e29a8",
+    "url": "/liquidflow/static/js/main.a9124e4c.chunk.js"
+  },
+  {
+    "revision": "fcbfe493c93947902445",
+    "url": "/liquidflow/static/js/1.fcbfe493.chunk.js"
+  },
+  {
+    "revision": "a9124e4cfa7a459e29a8",
+    "url": "/liquidflow/static/css/main.1574b764.chunk.css"
+  },
+  {
+    "revision": "285d038eb28be46c280a0eb197fc2777",
+    "url": "/liquidflow/index.html"
+  }
+];
