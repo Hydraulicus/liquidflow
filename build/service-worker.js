3316fcd6874c03bf3b8c7efbcedd8e81ac7960df
Alexei Zababurin <852gin@gmail.com>
2019-01-22 01:08:47 +0200

    Refining color of pipe Adding UI elements of flow options

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/liquidflow/precache-manifest.81aa7372d7487b19aa082d0834373c65.js"
+  "/liquidflow/build/precache-manifest.4927e5b9c94e87d712405eafd4395833.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/liquidflow/index.html", {
+workbox.routing.registerNavigationRoute("/liquidflow/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
